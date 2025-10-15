e9318204a67a03c7561f73cf4220393e173606a7
Mohamad <M.Beheshti148@gmail.com>
2025-10-15 20:55:56 +0330

    create user model

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, gmail, password=None):
+        if not username:
+            raise ValueError('نام کاربری الزامی است')
+        if not gmail:
+            raise ValueError('جیمیل الزامی است')
+
+        user = self.model(
+            username=username,
+            gmail=gmail,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, gmail, password=None):
+        user = self.create_user(
+            username=username,
+            password=password,
+            gmail=gmail
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    gmail = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['gmail']
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user'
